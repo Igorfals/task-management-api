c02df680935678713271c4b3156b9049aa3d5926
igorfals <igorfeals@gmail.com>
2025-06-15 20:15:49 -0300

    feat: implement authentication module with login functionality

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,10 +2,20 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { TaskModule } from './task/task.module';
+import { UsersModule } from './users/users.module';
+import { AuthController } from './auth/auth.controller';
+import { AuthService } from './auth/auth.service';
+import { AuthModule } from './auth/auth.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-    imports: [TaskModule],
-    controllers: [AppController],
-    providers: [AppService],
+    imports: [
+        ConfigModule.forRoot({ isGlobal: true }),
+        TaskModule,
+        UsersModule,
+        AuthModule,
+    ],
+    controllers: [AppController, AuthController],
+    providers: [AppService, AuthService],
 })
 export class AppModule {}
