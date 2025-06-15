a614f85de02e9d2f3a5bfa4b3988c006db6062ec
igorfals <igorfeals@gmail.com>
2025-06-15 03:49:06 -0300

    feat: implement task findByID

--- src/task/task.service.ts
+++ src/task/task.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
 import { TaskDto } from './task.dto';
 
 @Injectable()
@@ -7,5 +7,17 @@ export class TaskService {
     create(task: TaskDto) {
         this.tasks.push(task);
         console.log(this.tasks);
+    }
+
+    findByid(id: string): TaskDto {
+        const fundTask = this.tasks.filter((t) => t.id === id);
+        if (fundTask.length) {
+            return fundTask[0];
+        }
+
+        throw new HttpException(
+            `Task with id ${id} not found`,
+            HttpStatus.NOT_FOUND,
+        );
     }
 }
